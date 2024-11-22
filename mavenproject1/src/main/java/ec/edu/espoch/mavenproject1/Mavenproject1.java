ac2136723dbf2984cd6dd35fce1c1976cb4c1555
Stalyn0101 <yixon11@gmail.com>
2024-11-22 17:29:07 -0500

    hola

--- mavenproject1/src/main/java/ec/edu/espoch/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/ec/edu/espoch/mavenproject1/Mavenproject1.java
@@ -11,6 +11,6 @@ package ec.edu.espoch.mavenproject1;
 public class Mavenproject1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("pojsadsfpiojsdoifjsoajfoskjdsdaiudhasodhoais");
     }
 }
